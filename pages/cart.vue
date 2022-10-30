514dc94d6a5c9a10457fff76a4ec7dbdcaf9495b
Kelly Santos <kellycristinasntos@gmail.com>
2022-10-29 22:09:29 -0300

    Ajusted success purchase modal

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-[94vh] ">
+  <div class="h-[94vh]">
     <div class="flex flex-col items-center" v-if="cart.cartItems.length > 0">
       <div class="max-h-[60vh] w-10/12 overflow-y-scroll p-2">
         <div
@@ -7,10 +7,14 @@
           :key="item._id"
           class="flex my-2 h-28 bg-[#f2f2f2] w-full"
         >
-          <div class="flex  sm:w-32 mr-2"><img  class="w-32" :src="item.image" /></div>
+          <div class="flex sm:w-32 mr-2">
+            <img class="w-32" :src="item.image" />
+          </div>
           <div class="flex flex-col justify-evenly">
             <p class="text-lg font-bold capitalize">{{ item.name }}</p>
-            <div class="flex items-center justify-between  w-full sm:w-64 font-semibold">
+            <div
+              class="flex items-center justify-between w-full sm:w-64 font-semibold"
+            >
               <P class="text-[#ec483b] text-md"
                 >R${{ item.price.toFixed(2) }}</P
               >
@@ -21,13 +25,14 @@
         </div>
       </div>
       <div>
-        <div class="w-[60vw] flex flex-col text-xl my-8 ">
+        <div class="w-[60vw] flex flex-col text-xl my-8">
           <div class="flex justify-between border-b-4 border-dashed">
             <p class="font-bold">Total</p>
             <p>{{ cart.resume }}</p>
           </div>
 
-          <button @click="finish"
+          <button
+            @click="finish"
             class="bg-[#ec483b] text-white text-md h-10 leading-4 rounded-md p-2 lg:self-end mt-4"
           >
             Efetuar pagamento
@@ -45,6 +50,25 @@
       >
       <img src="../static/hungry-girl.png" alt="garota com fome" />
     </div>
+    <div
+      v-if="disabled"
+      class="absolute top-[6vh] h-[94vh] w-screen bg-black/60"
+    >
+      <div class="flex items-center justify-center h-full">
+        <div
+          class="flex flex-col justify-center items-center bg-white w-72 h-52 rounded-md"
+        >
+          <img class="w-16" src="@/static/icons/check-circle.gif" />
+
+          <p class="font-semibold text-lg">Transação bem sucedida!</p>
+          <span class="text-center pt-2">
+            <p class="text-sm"> Obrigada pela compra.</p>
+            <NuxtLink to="/" ><button class="rounded-md border-2 border-[#ec483b] p-2 my-4 w-52 text-[#ec483b] font-semibold">Voltar ao inicio</button> </NuxtLink>
+          
+          </span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -59,16 +83,46 @@ export default {
   computed: {
     ...mapState("products", ["cart"]),
   },
+  data() {
+    return {
+      disabled: false,
+    };
+  },
   methods: {
-    ...mapActions('products' ,["purchase"]),
+    ...mapActions("products", ["purchase"]),
     duplicate(index) {
       if (index == index) {
         ("ja tem esse no carrinho");
       }
     },
-    finish(){
-      this.purchase()
-    }
+    enableModal() {
+      this.disabled = true, this.purchase();
+    },
+    finish() {
+      setTimeout(
+      
+        this.enableModal
+  
+      , 2000);
+
+    },
+
   },
 };
 </script>
+<style scoped>
+::-webkit-scrollbar {
+  width: 8px;
+
+}
+
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px grey; 
+  border-radius: 10px;
+}
+ 
+::-webkit-scrollbar-thumb {
+  background: #ec483b; 
+  border-radius: 10px;
+}
+</style>
--- store/products/multations.js
+++ store/products/multations.js
@@ -13,30 +13,21 @@ const multations = {
         state.pagination.currentPage = data.page,
         state.pagination.totalPages = data.totalPages
     },
-    PAGE_PREV(state){
-        --state.pagina 
-    },
-    PAGE_NEXT(state){
-        ++state.pagina 
-    },
-SET_TO_CART(state, data){
+    SET_TO_CART(state, data){
     let items =  state.cart.cartItems
     let prices =  state.cart.amount
     items.push(data)
     prices.push(data.price)
     ++state.cart.quantity
-},
-SET_RESUME(state, resume){
+    },
+    SET_RESUME(state, resume){
     state.cart.resume = resume
-},
-FINALIZE_PURCHASE(state){
+    },
+    FINALIZE_PURCHASE(state){
     state.cart.quantity = 0,
     state.cart.resume = 0
     state.cart.cartItems = [],
     state.cart.amount = []
-}
-
-
-}
+}}
 
 export default multations
